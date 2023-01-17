8fd91fc59c1cb8149972aa19478d8d3f58b5a24a
hmchung2 <hmchung2@gmail.com>
2023-01-18 02:25:43 +0900

    commit

--- server.js
+++ server.js
@@ -1,39 +1,62 @@
+import { PrismaClient } from "@prisma/client";
 import { ApolloServer, gql } from "apollo-server";
+
+const client = new PrismaClient();
 
 // The GraphQL schema
 const typeDefs = gql`
   type Movie {
-    id: Int
-    title: String
-    year: Int
+    id: Int!
+    title: String!
+    year: Int!
+    genre: String
+    createdAt: String!
+    updatedAt: String!
   }
   type Query {
     movies: [Movie]
-    movie: Movie
+    movie(id: Int!): Movie
   }
 
   type Mutation {
-    createMovie(title: String!): Boolean
-    deleteMovie(title: String!): Boolean
+    createMovie(title: String!, year: Int!, genre: String): Movie
+    deleteMovie(id: Int!): Movie
+    updateMovie(id: Int!, year: Int!): Movie
   }
 `;
 
 // A map of functions which return data for the schema.
 const resolvers = {
   Query: {
-    movies: () => [],
-    movie: () => ({ title: "Hello", year: 2021 }),
+    movies: () => client.movie.findMany(),
+    movie: (_, { id }) =>
+      client.movie.findUnique({
+        where: {
+          id,
+        },
+      }),
   },
   Mutation: {
     // createMovie: (root, args, context, info) => "",
-    createMovie: (_, { title }) => {
-      console.log(title);
-      return true;
-    },
-    deleteMovie: (_, { title }) => {
-      console.log(title);
-      return true;
-    },
+    createMovie: (_, { title, year, genre }) =>
+      client.movie.create({
+        data: {
+          title,
+          year,
+          genre,
+        },
+      }),
+    deleteMovie: (_, { id }) =>
+      client.movie.delete({
+        where: { id },
+      }),
+    updateMovie: (_, { id, year }) =>
+      client.movie.update({
+        where: { id },
+        data: {
+          year,
+        },
+      }),
   },
 };
 
@@ -45,3 +68,18 @@ const server = new ApolloServer({
 server
   .listen()
   .then(() => console.log("Server is running on http://localhost:4000/"));
+
+// mutation{
+//   createMovie( title: "Nico Part 1" , genre : "Adventure" , year : 2022){
+//     title
+//     year
+//     createdAt
+//     updatedAt
+//   }
+// }
+
+// mutation{
+//   deleteMovie(id: 1 ){
+//     id
+//   }
+// }
