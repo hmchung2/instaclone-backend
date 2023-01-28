69f7fdf9d3ddf1ed21bc35d964c4bbc41eb466a0
hmchung2 <hmchung2@gmail.com>
2023-01-28 18:35:11 +0900

    follower pagination

--- users/seeFollowers/seeFollowers.resolvers.js
+++ users/seeFollowers/seeFollowers.resolvers.js
@@ -3,6 +3,18 @@ import client from "../../client";
 export default {
   Query: {
     seeFollowers: async (_, { userName, page }) => {
+      const ok = await client.user.findUnique({
+        where: { userName },
+        select: { id: true },
+      });
+
+      if (!ok) {
+        return {
+          ok: false,
+          error: "User not Found",
+        };
+      }
+
       const followers = await client.user
         .findUnique({
           where: { userName },
@@ -11,6 +23,10 @@ export default {
           take: 5,
           skip: (page - 1) * 5,
         });
+
+      const totalFollowers = await client.user.count({
+        where: { following: { some: { userName } } },
+      });
 
       //   const bFollowers = await client.user.findMany({
       //     where: {
@@ -27,6 +43,7 @@ export default {
       return {
         ok: true,
         followers,
+        totalPages: Math.ceil(totalFollowers / 5),
       };
     },
   },
