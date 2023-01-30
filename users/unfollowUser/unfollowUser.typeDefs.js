a4eb94c31bf36ff09cc57c528dc11d3164e5b6e9
hmchung2 <hmchung2@gmail.com>
2023-01-30 23:36:51 +0900

    edit comment and mutation response

--- users/unfollowUser/unfollowUser.typeDefs.js
+++ users/unfollowUser/unfollowUser.typeDefs.js
@@ -1,12 +1,7 @@
 import { gql } from "apollo-server";
 
 export default gql`
-  type unfollowUserResult {
-    ok: Boolean!
-    error: String
-  }
-
   type Mutation {
-    unfollowUser(userName: String!): unfollowUserResult
+    unfollowUser(userName: String!): MutationResponse!
   }
 `;
