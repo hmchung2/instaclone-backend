625bc8ceee207717c5263b4b697af36b52d22f92
hmchung2 <hmchung2@gmail.com>
2023-01-30 23:00:44 +0900

    comments

--- comments/deleteCommenet/deleteComment.typeDefs.js
+++ comments/deleteCommenet/deleteComment.typeDefs.js
@@ -1,12 +1,11 @@
 import { gql } from "apollo-server";
 
 export default gql`
-  type CreateCommentResult {
+  type DeleteCommentResult {
     ok: Boolean!
     error: String
   }
-
   type Mutation {
-    createComment(photoId: Int!, payload: String!): CreateCommentResult!
+    deleteComment(id: Int!): DeleteCommentResult!
   }
 `;
