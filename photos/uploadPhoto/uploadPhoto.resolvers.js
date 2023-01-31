4bad754462d1ba13979a37cc274b87ba9f9f5aa7
hmchung2 <hmchung2@gmail.com>
2023-02-01 02:21:10 +0900

    aws config s3 bucket

--- photos/uploadPhoto/uploadPhoto.resolvers.js
+++ photos/uploadPhoto/uploadPhoto.resolvers.js
@@ -1,4 +1,5 @@
 import client from "../../client";
+import { uploadToS3 } from "../../shared/shared.utils";
 import { protectedResolver } from "../../users/users.utils";
 import { processHashtags } from "../photos.utils";
 
@@ -10,16 +11,16 @@ export default {
         if (caption) {
           hashtagObj = processHashtags(caption);
         }
+        const fileUrl = await uploadToS3(file, loggedInUser.id, "uploads");
         return client.photo.create({
           data: {
-            file,
+            file: fileUrl,
             caption,
             user: {
               connect: {
                 id: loggedInUser.id,
               },
             },
-
             ...(hashtagObj.length > 0 && {
               hashtags: {
                 connectOrCreate: hashtagObj,
