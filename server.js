3b54e2e3b923eb358d1fe4bfd8786ac5eced0a69
hmchung2 <hmchung2@gmail.com>
2023-02-03 00:25:14 +0900

    real time pubsub

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
 require("dotenv").config();
+import http from "http";
 import express from "express";
 import logger from "morgan";
 import { ApolloServer } from "apollo-server-express";
 import { typeDefs, resolvers } from "./schema";
 import { getUser } from "./users/users.utils";
+// import pubsub from "./pubsub";
+
+// console.log(pubsub);
 
 const PORT = process.env.PORT;
 const apollo = new ApolloServer({
   resolvers,
   typeDefs,
   context: async ({ req }) => {
-    return {
-      loggedInUser: await getUser(req.headers.token),
-    };
+    if (req) {
+      return {
+        loggedInUser: await getUser(req.headers.token),
+      };
+    }
   },
 });
 
@@ -21,6 +27,9 @@ const app = express();
 app.use("/static", express.static("uploads"));
 apollo.applyMiddleware({ app });
 
-app.listen({ port: PORT }, () => {
+const httpServer = http.createServer(app);
+apollo.installSubscriptionHandlers(httpServer);
+
+httpServer.listen(PORT, () => {
   console.log(`🚀Server is running on http://localhost:${PORT}/graphql ✅`);
 });
